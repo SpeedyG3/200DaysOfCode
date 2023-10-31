762d81b231162227f17d3e724f96f1828070052c
Hrishi <hrishikeshkulkarni009@gmail.com>
2023-11-01 00:10:36 +0530

    Day57

--- Test.java
+++ Test.java
@@ -1,32 +1,19 @@
-import java.util.*;
+import java.util.Arrays;
 public class Test{
-    public static int minOperations(ArrayList<Integer> nums, int k) {
-        int arr[] = new int[k];
-        for(int i=0;i<k;i++){
-            arr[i] = i+1;
-        }
-        int ct=0;
-        int ans=0;
-        for(int j=nums.size()-1;j>=0;j--){
-            if(nums.get(j)<=k && nums.get(j)>0 && arr[nums.get(j)-1]!=-1){
-                arr[nums.get(j)-1]=-1;
+    public static int flipper(String t){
+        char curr = '0';
+        int ct = 0;
+        for (int i=0;i<t.length();i++) {
+            if (t.charAt(i)!=curr) {
                 ct++;
-            }
-            if(ct==k && ans==0){
-                ans=nums.size()-j-1;
-                break;
+                curr = (char)(((curr + 1)%2)+48);
             }
         }
-        return ans;
+        return ct;
     }
+ 
     public static void main(String args[]){
-        ArrayList<Integer> nums = new ArrayList<>();
-        nums.add(3);
-        nums.add(1);
-        nums.add(5);
-        nums.add(4);
-        nums.add(3);
-        int k = 5;
-        System.out.println(minOperations(nums, k));
+        String t = "010110000000011111111111111111100101001010101111";
+        System.out.println(flipper(t));
     }
 }
