2639d5ead958d9d75c41d3182ca0708846b7d20e
Hrishi <hrishikeshkulkarni009@gmail.com>
2024-03-02 23:44:32 +0530

    Day176n177

--- Test.java
+++ Test.java
@@ -107,73 +107,159 @@
 //     }
 // }
 
+// import java.util.*;
+// public class Test{
+//     public static class Node{
+//         int val;
+//         Node next;
+//         Node prev; 
+
+//         public Node(int v){
+//             this.val = v;
+//             this.next = null;
+//             this.prev = null;
+//         }
+//     }
+
+//     public static Node addLast(Node head, int v){
+//         Node newNode = new Node(v);
+//         if(head==null){
+//             return newNode;
+//         }else{
+//             Node temp = head;
+//             while(temp.next!=null){
+//                 temp = temp.next;
+//             }
+//             temp.next = newNode;
+//             newNode.prev = temp;
+//             return head;
+//         }
+//     }
+
+//     public static void printer(Node head){
+//         Node temp = head;
+//         while(temp!=null){
+//             System.out.print(temp.val+" ");
+//             temp = temp.next;
+//         }
+//         System.out.println();
+//     }
+
+//     public static Node makeSortedList(Node head){
+//         if(head==null || head.next==null){
+//             return head;
+//         }
+//         Node temp = head;
+//         Node nextNode;
+//         while(temp!=null && temp.next!=null){
+//             nextNode = temp.next;
+//             while(nextNode!=null && nextNode.val==temp.val){
+//                 nextNode = nextNode.next;
+//             }
+//             temp.next = nextNode;
+//             if(nextNode!=null){
+//                 nextNode.prev = temp;
+//             }
+//             temp = temp.next;
+//         }
+//         return head;
+//     }
+    
+//     public static void main(String args[]){
+//         Test ll = new Test();
+//         Node head = null;
+//         int arr[] = {1,1,1,2,3,3,4};
+//         for(int i=0;i<arr.length;i++){
+//             head = addLast(head, arr[i]);
+//         }
+//         printer(head);
+//         head = makeSortedList(head);
+//         printer(head);
+//     }
+// }
+
+//binary strings w/o consecutive ones
+// import java.util.*;
+// public class Test{
+//     public static void recSoln(List<String> ans, int i, int N, String s){
+//         if(i==N){
+//             ans.add(s.toString());
+//             return;
+//         }
+
+//         if(i==0){
+//             recSoln(ans, i+1, N, s+"1");
+//             recSoln(ans, i+1, N, s+"0");
+//         }else{
+//             char prev = s.charAt(i-1);
+//             if(prev!='1'){
+//                 recSoln(ans, i+1, N, s+"1");
+//             }
+//             recSoln(ans, i+1, N, s+"0");
+//         }
+//     }
+
+//     public static List<String> generateString(int N){
+//         List<String> ans = new ArrayList<>();
+//         recSoln(ans, 0, N, new String(""));
+//         return ans;
+//     }
+    
+//     public static void main(String args[]){
+//         int N = 3;
+//         List<String> ans = generateString(N);
+//         for(String s: ans){
+//             System.out.print(s+" ");
+//         }
+//     }
+// }
+
+//more subsequence 
+// 	Learn All Patterns of Subsequences …
+// https://www.codingninjas.com/codestudio/problems/more-subsequence_8842355?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
 import java.util.*;
-public class Test{
-    public static class Node{
-        int val;
-        Node next;
-        Node prev; 
 
-        public Node(int v){
-            this.val = v;
-            this.next = null;
-            this.prev = null;
+public class Test {
+    public static void recSoln(int n, int i, String s, String o, List<String> ans) {
+        if (i == n) {
+            ans.add(s);
+            return;
+        }
+
+        if (i == 0) {
+            recSoln(n, i + 1, s + o.charAt(i), o, ans);
+        } else {
+            recSoln(n, i + 1, s, o, ans);
+            recSoln(n, i + 1, s + o.charAt(i), o, ans);
         }
     }
 
-    public static Node addLast(Node head, int v){
-        Node newNode = new Node(v);
-        if(head==null){
-            return newNode;
-        }else{
-            Node temp = head;
-            while(temp.next!=null){
-                temp = temp.next;
-            }
-            temp.next = newNode;
-            newNode.prev = temp;
-            return head;
-        }
-    }
+    public static void main(String args[]) {
+        int n = 2;
+        int m = 2;
+        String a = "ab";
+        String b = "dd";
 
-    public static void printer(Node head){
-        Node temp = head;
-        while(temp!=null){
-            System.out.print(temp.val+" ");
-            temp = temp.next;
+        List<String> subA = new ArrayList<>();
+        recSoln(n, 0, "", a, subA);
+        for(String s: subA){
+            System.out.print(s+" ");
         }
         System.out.println();
-    }
-
-    public static Node makeSortedList(Node head){
-        if(head==null || head.next==null){
-            return head;
+        int ctA = subA.size();
+        
+        List<String> subB = new ArrayList<>();
+        recSoln(m, 0, "", b, subB);
+        for(String s: subB){
+            System.out.print(s+" ");
         }
-        Node temp = head;
-        Node nextNode;
-        while(temp!=null && temp.next!=null){
-            nextNode = temp.next;
-            while(nextNode!=null && nextNode.val==temp.val){
-                nextNode = nextNode.next;
-            }
-            temp.next = nextNode;
-            if(nextNode!=null){
-                nextNode.prev = temp;
-            }
-            temp = temp.next;
+        System.out.println();
+        int ctB = subB.size();
+        
+        if (ctA >= ctB) {
+            System.out.println("A");
+        } else {
+            System.out.println("B");
         }
-        return head;
-    }
-    
-    public static void main(String args[]){
-        Test ll = new Test();
-        Node head = null;
-        int arr[] = {1,1,1,2,3,3,4};
-        for(int i=0;i<arr.length;i++){
-            head = addLast(head, arr[i]);
-        }
-        printer(head);
-        head = makeSortedList(head);
-        printer(head);
     }
 }
